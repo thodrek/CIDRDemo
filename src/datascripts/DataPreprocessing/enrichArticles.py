5e74c9ec398eb8675813711d6ae30f61c24dea5e
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-12-13 00:34:16 -0500

    changed key set

--- src/datascripts/DataPreprocessing/enrichArticles.py
+++ src/datascripts/DataPreprocessing/enrichArticles.py
@@ -3,7 +3,7 @@ __author__ = 'thodrek'
 import cPickle as pickle
 import argparse
 import datetime
-from DataPreprocessing.calais import Calais
+from DataPreprocessing import Calais
 import socket
 import os
 import sys
