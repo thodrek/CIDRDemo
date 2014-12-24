2a6588ce549c2b3b96e233981b0b9b6e2ac4dae9
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-12-23 21:20:21 -0500

    possitioning

--- src/datascripts/scripts/srcsel/barPlot.js
+++ src/datascripts/scripts/srcsel/barPlot.js
@@ -62,7 +62,14 @@ function plotBars(data) {
 	  svg.append("g")
 	      .attr("class", "x axis")
 	      .attr("transform", "translate(0," + height + ")")
-	      .call(xAxis);
+	      .call(xAxis)
+	      .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-65)"
+                });
 
 	  svg.append("g")
 	      .attr("class", "y axis")
